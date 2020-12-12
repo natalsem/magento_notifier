340ae892b151d05a77cf16c597b3904ad8080bfc
Natalia Sekulich <sekulich.n@gmail.com>
2020-12-12 19:49:03 +0300

    Initial commit

---
+++ registration.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @author Natalia Sekulich <sekulich.n@gmail.com>
+ */
+declare(strict_types=1);
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Natalsem_Notification',
+    __DIR__
+);
